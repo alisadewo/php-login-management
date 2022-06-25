6c87ed47d07014c7de80a60505560a5320a7ee72
alisadewo <sadewoali13@gmail.com>
2022-06-25 10:44:31 +0700

    Add File Template.html Footer.php Header.php, Modify File Config/View.php HomeController.php View/Home/Index.php public/index.php, Delete File ProductController.php AuthMiddleware.php View/index.php View/login.php  View/register.php Hello.php tests/RegexTest.php

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -7,51 +7,11 @@ use Als\Belajar\PHP\MVC\App\View;
 class HomeController
 {
 
-	function index(): void
+	function index()
 	{
-		$model = [
-			"title" => "Belajar PHP MVC",
-			"content" => "Selamat Belajar PHP MVC dari Programmer Zaman Now",
-		];
-
-		// require __DIR__ . '/../View/Home/index.php';
-		View::render('Home/index', $model);
-
+		View::render('Home/index', [
+			"title" => "PHP Login Management"
+		]); 
 	}
-
-	function hello(): void
-	{
-		echo "HomeController.hello()";
-	}
-
-	function world(): void
-	{
-		echo "HomeController.world()";
-	}
-
-	function about(): void
-	{
-		echo "Author: Ali Sadewo";
-	}
-
-	function login()
-	{
-		$request = [
-			"username" => $_POST['username'],
-			"password" => $_POST['password'],
-		];
-
-		$user = [
-
-		];
-
-
-		$response = [
-			"message" => "Login Sukses"
-		];
-
-		// Kirimkan response ke View
-	}
-
 
 }

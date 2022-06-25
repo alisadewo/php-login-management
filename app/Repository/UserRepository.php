74271ee90b117c732a69251e0f104db025dcab42
alisadewo <sadewoali13@gmail.com>
2022-06-25 17:21:33 +0700

    Implemention Unit Test User Registration Repository

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -16,12 +16,40 @@ class UserRepository
 
 	public function save(User $user): User
 	{	
-		$sql = "INSERT INTO users(id, name, password) VALUES(?, ?, ?)"
+		$sql = "INSERT INTO users(id, name, password) VALUES(?, ?, ?)";
 		$statement = $this->connection->prepare($sql);
 		$statement->execute([
 			$user->id, $user->name, $user->password
 		]);
 		return $user;
+	}
+
+	public function findById(string $id): ?User
+	{
+		$sql = "SELECT id, name, password FROM users WHERE id = ?";
+		$statement = $this->connection->prepare($sql);
+		$statement->execute([$id]);
+
+		try{
+			if($row = $statement->fetch())
+			{
+				$user = new User();
+				$user->id = $row['id'];
+				$user->name = $row['name'];
+				$user->password = $row['password'];
+				return $user;
+			} else {
+				return null;
+			}
+		} finally {
+			$statement->closeCursor();
+		}
+	}
+
+	public function deleteAll():void
+	{
+		$sql = "DELETE FROM users";
+		$this->connection->exec($sql);
 	}
 
 }
---
+++ tests/Repository/UserRepositoryTest.php
@@ -0,0 +1,43 @@
+<?php 
+
+namespace Als\Belajar\PHP\MVC\Repository;
+
+use PHPUnit\Framework\TestCase;
+use Als\Belajar\PHP\MVC\Config\Database;
+use Als\Belajar\PHP\MVC\Domain\User;
+
+class UserRepositoryTest extends TestCase
+{
+
+	private UserRepository $userRepository;
+
+	protected function setUp(): void
+	{
+		$connection = Database::getConnection();
+		$this->userRepository = new UserRepository($connection);
+
+		$this->userRepository->deleteAll();
+	}
+
+	public function testSaveSuccess()
+	{
+		$user = new User();
+		$user->id = "ali123";
+		$user->name = "Ali";
+		$user->password = "rahasia";
+
+		$this->userRepository->save($user);
+
+		$result = $this->userRepository->findById($user->id);
+
+		self::assertEquals($user->id, $result->id);
+		self::assertEquals($user->name, $result->name);
+		self::assertEquals($user->password, $result->password);
+	}
+
+	public function testFindByIdNotFound()
+	{
+		$user = $this->userRepository->findById("notfound");
+		self::assertNull($user);
+	}
+}

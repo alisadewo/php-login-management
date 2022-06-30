85c98bc76e8cd54955051cd4629f561f525d3ba0
alisadewo <sadewoali13@gmail.com>
2022-06-30 16:07:37 +0700

    Add Session Middleware

---
+++ app/Middleware/MustLoginMiddleware.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace Als\Belajar\PHP\MVC\Middleware;
+
+use Als\Belajar\PHP\MVC\App\View;
+use Als\Belajar\PHP\MVC\Config\Database;
+use Als\Belajar\PHP\MVC\Repository\SessionRepository;
+use Als\Belajar\PHP\MVC\Repository\UserRepository;
+use Als\Belajar\PHP\MVC\Service\SessionService;
+
+class MustLoginMiddleware implements Middleware
+{
+	private SessionService $sessionService;
+
+	public function __construct()
+	{
+		$sessionRepository = new SessionRepository(Database::getConnection());
+		$userRepository = new UserRepository(Database::getConnection());
+		$this->sessionService = new SessionService($sessionRepository, $userRepository);
+	}
+
+	function before(): void
+	{
+		$user = $this->sessionService->current();
+		if($user == null) {
+			View::redirect('/users/login');
+		}
+	}
+}
